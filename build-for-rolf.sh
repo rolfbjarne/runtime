0e86c8fc229bfdf1118816bd4782b86c58463337
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-03-24 10:56:47 +0100

    arm64

--- build-for-rolf.sh
+++ build-for-rolf.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -eux
 
 (
+    git clean -xfd
+    ./build.sh clr+libs+host+packs+mono -configuration release -arch arm64 -cross /p:UseCrossGen2=false
+    ./build.sh libs+host+packs+mono -configuration release -arch arm64 -cross /p:UseCrossGen2=false
     ./build.sh clr+libs+host+packs+mono -configuration release
     ./build.sh libs+host+packs+mono -configuration release
 
