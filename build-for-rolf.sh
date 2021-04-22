93234e97a5078fe79226450582cff95173fc4cad
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-04-22 13:57:10 +0200

    I'm curious...

--- build-for-rolf.sh
+++ build-for-rolf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 
-(
+time (
+    set -x
+    set -e
     git clean -xfd
     ./build.sh clr+libs+host+packs+mono -configuration release -arch arm64 -cross /p:UseCrossGen2=false
     ./build.sh libs+host+packs+mono -configuration release -arch arm64 -cross /p:UseCrossGen2=false
